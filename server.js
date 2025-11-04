72286ad184fcb2720eee0b246956fd10cb98b993
Onekamer <contact@onekamer.co>
2025-11-04 22:16:38 +0100

    Change dotenv config path for environment variables

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 import * as dotenv from "dotenv";
-dotenv.config({ path: ".env.local" });
+dotenv.config({ path: "./Onekamer-Server-Lab.env" });
 
 import express from "express";
 import Stripe from "stripe";
